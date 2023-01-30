a55f45febcd68d1790ac18a3a3f2499bb9a38431
satish <srgsatishsrg@gmail.com>
2023-01-30 17:36:40 +0530

    updated with some removed comments

--- src/assets/script.js
+++ src/assets/script.js
@@ -1,11 +1,7 @@
-var firstTime = true;
 
 function update(code_input, outputRenderTag) {
+  alert("why you called me? update()");
   const output = document.getElementById(outputRenderTag);
-  if(firstTime){
-    firstTime = false
-    return
-  }else{
     const s = document.createElement('script');
     s.setAttribute('type','text/tikz');
     s.textContent = `
@@ -17,36 +13,4 @@ function update(code_input, outputRenderTag) {
     output.appendChild(s);
     process_tikz(s);
   }
-}
 
-let debounce_update = null;
-let debounce_do = false;
-
-
-function renderMe(){
-  const code_input = document.getElementById('code');
-  const output = document.getElementById('output');
-  if(firstTime){
-    firstTime = false
-    return
-  }else{
-    alert("render clicked");
-    if(debounce_update) {
-      debounce_do = true;
-      return;
-    }
-    update();
-    debounce_update = setTimeout(function() {
-      debounce_update = null;
-      if(debounce_do) {
-        update();
-      }
-      debounce_do = false;
-    },5000); 
-  }
-  
-}
-
-
-
-update();
